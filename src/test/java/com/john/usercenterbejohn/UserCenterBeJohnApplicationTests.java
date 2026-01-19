1e59554a53e41d9100deef7c3a7500df94cd5e32
duqingyang <s202310001011@163.com>
2026-01-19 16:40:09 +0800

    lesson2: test if can use db

--- src/test/java/com/john/usercenterbejohn/UserCenterBeJohnApplicationTests.java
+++ src/test/java/com/john/usercenterbejohn/UserCenterBeJohnApplicationTests.java
@@ -1,13 +1,31 @@
 package com.john.usercenterbejohn;
 
+import com.john.usercenterbejohn.demos.web.mapper.UserMapper;
+import com.john.usercenterbejohn.demos.web.model.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
+//@RunWith(SpringRunner.class)
 class UserCenterBeJohnApplicationTests {
 
+    @Resource
+    UserMapper userMapper;
+
     @Test
-    void contextLoads() {
+    void testUser() {
+        System.out.println("----test user select----");
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(2,userList.size());
+        userList.forEach(System.out::println);
+
     }
 
 }
